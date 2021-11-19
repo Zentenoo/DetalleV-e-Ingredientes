c0104170a4b3dcd1f01d9ee57056afd3f6bdf3d3
Mateo <94015253+Zentenoo@users.noreply.github.com>
2021-11-18 23:45:00 -0400

    Add files via upload

---
+++ DetalleVentaAD.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using System.Data.SqlClient;
+
+namespace CapaAccesoDatos
+{
+    class DetalleVentaAD
+    {
+        public void CrearDetalleVenta (DetalleVentaEntidad detalleVenta)
+        {
+            using (SqlConnection conn = new SqlConnection(ConexionSQL.ObtenerCadenaConexion()))
+            {
+                conn.Open();
+
+                string sqlDetalleV = @"INSERT INTO tblDetalleVenta(lIdVenta,lCantidad_Platos2,
+                                         lPrecio_Subtotal, lPrecio_Total, 
+                                         sDescripcionV) VALUES (@lIdVenta, @)lDescuento, 
+                                         @lCantidad_Platos2, @lPrecio_Subtotal, @lPrecio_Total
+                                         @sDescripcionV) SELECT SCOPE_IDENTITY()";
+
+                using (SqlCommand cmd= new SqlCommand(sqlDetalleV,conn))
+                {
+                    cmd.Parameters.AddWithValue("@lIdVenta", detalleVenta.lIdVenta);
+                    cmd.Parameters.AddWithValue("@lCantidad_Platos2", detalleVenta.lCantidad_Platos2 );
+                    cmd.Parameters.AddWithValue("@lPrecio_Subtotal", detalleVenta.lPrecio_Subtotal);
+                    cmd.Parameters.AddWithValue("@lPrecio_Total",detalleVenta.lPrecio_Total);
+                    cmd.Parameters.AddWithValue("@sDescripcionV",detalleVenta.sDescripcionV);
+
+                    detalleVenta.lIdVenta = Convert.ToInt32(cmd.ExecuteScalar());
+                }
+
+                string sqlLineaDetalle = @"INSERT INTO "
+            }
+        }
+    }
+}
---
+++ IngredientesAD.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+namespace CapaAccesoDatos
+{
+    class IngredientesAD
+    {
+        /// <summary>
+        /// Busca Ingredientes por su ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public IngredientesEntidad BuscarPorId(int id)
+        {
+            IngredientesEntidad ingrediente = null;
+            using (SqlConnection conn = new SqlConnection(ConexionSQL.ObtenerCadenaConexion()))
+            {
+                conn.Open();
+
+                string sql = @"SELECT lIdIngrediente, lIdComida, sNombreIngr 
+                                FROM tblIngredientes 
+                                WHERE lIdIngrediente = @lIdIngrediente";
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("lIdIngrediente", id);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    ingrediente = CargarIngredientes(reader);
+                }
+            }
+
+            return ingrediente;
+        }
+
+        /// <summary>
+        /// Auxiliar
+        /// </summary>
+        /// <param name="reader"></param>
+        /// <returns></returns>
+        private IngredientesEntidad CargarIngredientes(IDataReader reader)
+        {
+            IngredientesEntidad ingrediente = new IngredientesEntidad();
+
+            ingrediente.lIdIngrediente = Convert.ToInt32(reader["lIdIngrediente"]);
+            ingrediente.sNombreIngr = Convert.ToString(reader["sNombreIngr"]);
+            ingrediente.lIdComida = Convert.ToInt32(reader["lIdComida"]);
+
+
+            return ingrediente;
+        }
+    }
+}
